3d63eec34a756fac78beb0fe33789a02c5f69ac4
lexandrovitsch <rfvghm@gmail.com>
2024-06-11 17:20:34 +0300

    Average score (translated)

--- py/ancient_trash/Average score.py
+++ py/ancient_trash/Average score.py
@@ -1,10 +1,11 @@
-def from_tiny_to_large (avscore, drscore, count, mark):
-    counter = 0 # Счётчик
+"""Main def of increasing mark"""
+def main (avscore, drscore, count, mark):
+    counter = 0 # Counter
     if (avscore > 5 or avscore < 2) or (drscore > 5 or drscore < 2):
-        return 'Error #1: Ср. балл или желаемый балл ∉ [2; 5]'
-    if avscore >= drscore: # Исключение ненужной работы
+        return 'Error: Average score and/or dream score ∉ [2; 5]'
+    if avscore >= drscore: # Exception extra actions
         return 0
-    else: # Основной алгоритм
+    else: # Main algorithm
         while avscore <= drscore:
             summ = avscore * count
             summ += mark
@@ -14,16 +15,16 @@ def from_tiny_to_large (avscore, drscore, count, mark):
     return counter
 
 
-#Сбор данных
-average_score = float(input('Введите ср. балл >> '))
-dream_score = float(input('Введите желаемый ср. балл >> '))
-count_marks = int(input('Введите количество оценок >> '))
-mark_for_solve = int(input('Введите оценку для исправления >> '))
+"""Collecting data"""
+average_score = float(input('Enter average score >> '))  # Current average score
+dream_score = float(input('Enter \"dream\" score >> '))  # "Dream" score
+count_marks = int(input('Enter marks count >> '))  #  Current marks count
+mark_for_solve = int(input('Enter mark for solving >> '))  # Mark that will be used for solving
 
-# Исключение бесконечного цикла
+"""Exception of infinite cycle"""
 if dream_score % 1 == 0:
-    round_number = 1 - float(input('Введите минимальную дробную часть для огругления итоговой оценки в большую сторону >> '))
+    round_number = 1 - float(input('Enter minimal decimal part for rounding your mark >> '))
     dream_score -= round_number
 
-# Использование ф-ции "from_tiny_to_large" и вывод результата на экран
-print(from_tiny_to_large(average_score, dream_score, count_marks, mark_for_solve))
+# Output of the result using 
+print(main(average_score, dream_score, count_marks, mark_for_solve))
