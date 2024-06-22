ba8634128bba3133c88810a16a034c553dff073d
lexaDOOM <rfvghm@gmail.com>
2024-06-22 12:43:06 +0300

    'lab7.py' - added input and output data length measurement, some cosmetic changes

--- py/1_course/tic_labs/lab7.py
+++ py/1_course/tic_labs/lab7.py
@@ -7,13 +7,16 @@ def main_lz77(input_file, output_file):
     """LZ77 main def"""
     lz77 = LZ77(window_size=20)
     data = read_file(input_file)
+    input_length_lz77 = len(data)  # Input data length measurement
 
     lz77_compress_s_time = timeit.default_timer()  # LZ77 compress start time
     compressed = lz77.compress_lz77(data)
     lz77_compress_total_time = timeit.default_timer() - lz77_compress_s_time  # LZ77 compress total time
+    compressed_length_lz77 = len(compressed)  # Compressed data length measurement
 
     write_compressed_file(output_file, compressed)
-    print(f'Compressed data written to {output_file}\nTime elapsed: {lz77_compress_total_time}')
+    print(f'\nCompressed data written to {output_file}\nTime elapsed: {lz77_compress_total_time}\n\
+Input data length: {input_length_lz77}\nCompressed data length: {compressed_length_lz77}')
     
 
 def decompress_file_lz77(input_file, output_file):
@@ -27,7 +30,7 @@ def decompress_file_lz77(input_file, output_file):
 
     with open(output_file, 'w') as file:
         file.write(decompressed)
-    print(f'Decompressed data written to {output_file}\nTime elapsed: {lz77_decompress_total_time}')
+    print(f'\nDecompressed data written to {output_file}\nTime elapsed: {lz77_decompress_total_time}')
 #-------------------------------------------------------
 
 
@@ -36,13 +39,16 @@ def main_lz78(input_file, output_file):
     """LZ78 main def"""
     lz78 = LZ78()
     data = read_file(input_file)
+    input_length_lz78 = len(data)  # Input data length measurement
 
     lz78_compress_s_time = timeit.default_timer()  # LZ78 compress start time
     compressed = lz78.compress_lz78(data)
     lz78_compress_total_time = timeit.default_timer() - lz78_compress_s_time  # LZ78 compress total time
+    compressed_length_lz78 = len(compressed)  # Compressed data length measurement
 
     write_compressed_file(output_file, compressed)
-    print(f'Compressed data written to {output_file}\nTime elapsed: {lz78_compress_total_time}')
+    print(f'\nCompressed data written to {output_file}\nTime elapsed: {lz78_compress_total_time}\n\
+Input data length: {input_length_lz78}\nCompressed data length: {compressed_length_lz78}')
 
 
 def decompress_file_lz78(input_file, output_file):
@@ -55,7 +61,7 @@ def decompress_file_lz78(input_file, output_file):
     lz78_decompress_total_time = timeit.default_timer() - lz78_decompress_s_time  # LZ78 decompress total time
 
     write_decompressed_file(output_file, decompressed)
-    print(f'Decompressed data written to {output_file}\nTime elapsed: {lz78_decompress_total_time}')
+    print(f'\nDecompressed data written to {output_file}\nTime elapsed: {lz78_decompress_total_time}')
 #-------------------------------------------------------
 
 
@@ -64,13 +70,16 @@ def main_lzw(input_file, output_file):
     """LZW main def"""
     lzw = LZW()
     data = read_file(input_file)
+    input_length_lzw = len(data)  # Input data length measurement
 
     lzw_compress_s_time = timeit.default_timer()  # LZW compress start time
     compressed = lzw.compress_lzw(data)
     lzw_compress_total_time = timeit.default_timer() - lzw_compress_s_time  # LZW compress total time
+    compressed_length_lzw = len(compressed)  # Compressed data length measurement
 
     write_compressed_file(output_file, compressed)
-    print(f'Compressed data written to {output_file}\nTime elapsed: {lzw_compress_total_time}')
+    print(f'\nCompressed data written to {output_file}\nTime elapsed: {lzw_compress_total_time}\n\
+Input data length: {input_length_lzw}\nCompressed data length: {compressed_length_lzw}')
 
 
 def decompress_file_lzw(input_file, output_file):
@@ -83,7 +92,7 @@ def decompress_file_lzw(input_file, output_file):
     lzw_decompress_total_time = timeit.default_timer() - lzw_decompress_s_time  # LZW decompress total time
 
     write_decompressed_file(output_file, decompressed)
-    print(f'Decompressed data written to {output_file}\nTime elapsed: {lzw_decompress_total_time}')
+    print(f'\nDecompressed data written to {output_file}\nTime elapsed: {lzw_decompress_total_time}')
 #-------------------------------------------------------
 
 # Initialization
